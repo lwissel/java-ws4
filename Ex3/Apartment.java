b889b2416f728fd8f7e9b724d454c689cbcb4192
lwissel <lennart.wissel@gmx.net>
2014-11-27 20:15:16 +0000

    add javadoc

--- Ex3/Apartment.java
+++ Ex3/Apartment.java
@@ -7,7 +7,13 @@
  */
 
 public class Apartment extends House {
-  // constructor
+  /** full constructor to be extended in case
+   * @param type s ellable or r entable
+   * @param bedrooms number of bedrooms
+   * @param description what it says
+   * @param picture string containing the filepath
+   * @param payAmount is the money, value, rent whatever referring to the object
+   */
   public Apartment(char type, int bedrooms, String description, String picture, double payAmount) {
     super(type, bedrooms, description, picture, payAmount);
   }
